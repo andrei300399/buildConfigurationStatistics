6b3acf989dd41d7a6da2d3e6130f5960b39a848c
Kukhto <Andrey.Kukhto@nexign.com>
2024-01-07 23:58:33 +0300

    add test convert time

--- src/test/java/io/jenkins/plugins/sample/BuildConfigurationStatisticsBuilderTest.java
+++ src/test/java/io/jenkins/plugins/sample/BuildConfigurationStatisticsBuilderTest.java
@@ -8,13 +8,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class BuildConfigurationStatisticsBuilderTest {
 
     @Rule
     public JenkinsRule jenkins = new JenkinsRule();
 
     @Test
-    public void testWorkingSystem() throws Exception {
+    public void testWorkingSystem() {
         assert 1 == 1;
     }
 
@@ -30,5 +36,23 @@ public class BuildConfigurationStatisticsBuilderTest {
         FreeStyleProject project = jenkins.createFreeStyleProject();
         project.getBuildersList().add(new Shell("echo1 hello"));
         jenkins.buildAndAssertStatus(Result.FAILURE, project);
+    }
+
+    @Test
+    public void testConvertLongTimeToDate() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = dateFormat.parse("23/09/2007");
+        long time = date.getTime();
+        long resultDate = DateTimeHandler.convertDateToLongTime(date);
+        assert resultDate == time;
+    }
+
+    @Test
+    public void testConvertDateToLongTime() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = dateFormat.parse("23/09/2007");
+        long time = date.getTime();
+        Date resultDate = DateTimeHandler.convertLongTimeToDate(time);
+        assert resultDate.equals(date);
     }
 }
