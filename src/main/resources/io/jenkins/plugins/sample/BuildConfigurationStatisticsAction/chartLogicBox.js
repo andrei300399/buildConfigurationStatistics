ebb18c9d2ecc7d1b273ebeec84bd78c9304ed7c8
Kukhto <Andrey.Kukhto@nexign.com>
2024-01-15 01:49:41 +0300

    add types diagramm

--- src/main/resources/io/jenkins/plugins/sample/BuildConfigurationStatisticsAction/chartLogicBox.js
+++ src/main/resources/io/jenkins/plugins/sample/BuildConfigurationStatisticsAction/chartLogicBox.js
@@ -1,27 +1,104 @@
-//var scr1 = document.getElementById("script1");
-//console.log("ddddd", scr1);
-//const labels = Array.from({length: 30}, (_, i) => i + 1);
-//const dataTestCount = {
-//  labels: labels,
-//  datasets: [{
-//    label: 'Test Count',
-//    data: Array.from({length: 30}, () => Math.floor(Math.random() * 60)),
-//    borderColor: [
-//      'rgba(0, 180, 33, 1)',
-//    ],
-//    tension: 0.1
-//
-//  }]
-//};
-//var settingsTestCount = {
-//                        type: 'line',
-//                        data: dataTestCount,
-//                      };
+
+
+
+
 var ctx = document.getElementById("successRateChart").getContext("2d");
 var ctxBuild = document.getElementById("buildDurationChart").getContext("2d");
 var ctxArtifactsSize = document.getElementById("artifactsSize").getContext("2d");
 var ctxTimeSpentQueue = document.getElementById("timeSpentQueue").getContext("2d");
 var ctxTestCount = document.getElementById("testCount").getContext("2d");
+
+
+function typeChartHandler(typeChart, labels, title, dictValues){
+
+  let data = {};
+  let allPerf = {};
+  switch(typeChart) {
+      case 'Bar':
+        data = {
+                  labels: labels,
+                  datasets: [{
+                    label: title,
+                    data: dictValues,
+                    backgroundColor: [
+                      'rgba(0, 255, 0, 0.5)',
+                    ],
+                    borderColor: [
+                      'rgb(0, 69, 36)',
+                    ],
+                    categoryPercentage: 1,
+                    borderWidth: 1,
+                    barPercentage: 1,
+                  }]
+                };
+       allPerf = {
+                                type: 'bar',
+                                data: data,
+                                options: {
+                                    scales: {
+                                          y: {
+                                            beginAtZero: true
+                                          }
+                                        }
+                                }
+                              };
+
+      break;
+      case 'Line':
+      data = {
+        labels: labels,
+        datasets: [{
+          label: title,
+          data: dictValues,
+          borderColor: [
+            'rgba(0, 180, 33, 1)',
+          ],
+          tension: 0.1
+
+        }]
+      };
+
+      allPerf = {
+                              type: 'line',
+                              data: data,
+
+                            };
+
+
+      break;
+
+      case 'Scatter':
+
+            data = {
+              labels: labels,
+              datasets: [{
+                label: title,
+                data: dictValues,
+                borderColor: [
+                  'rgba(0, 180, 33, 1)',
+                ],
+                tension: 0.1
+
+              }]
+            };
+
+            allPerf = {
+                                    type: 'radar',
+                                    data: data,
+                                    options:  {
+                                                     scale: {
+                                                         min: 0
+                                                     },
+                                                 },
+                                  };
+
+
+      break;
+
+  }
+
+return allPerf;
+}
 
 
 function formatLabelsDate(arrLabels, dateFormat, period) {
@@ -85,8 +162,11 @@ function sortOnKeys(dict, period) {
 
 var successRateSelect = document.querySelector("#selectSuccess");
 
-function createSuccessRateChart(period){
-console.log("period", period)
+function createSuccessRateChart(){
+var period = document.getElementById("selectSuccess").value;
+var typeChart = document.getElementById("selectChartSR").value;
+console.log("period", period);
+console.log("typeChart", typeChart)
 var successRate2 = document.querySelector("#successRateData").textContent;
   console.log(successRate2);
   var obj = JSON.parse(successRate2);
@@ -106,33 +186,9 @@ dataSuccessRateDict[Date.parse(key)] = parseFloat(obj[key]);
   dictSuccess = sortOnKeys(dataSuccessRateDict, period)[0];
   labelsSuccess = sortOnKeys(dataSuccessRateDict, period)[1];
 
-  const data = {
-    labels: labelsSuccess,
-    datasets: [{
-      label: 'Success rate',
-      data: dictSuccess,
-      backgroundColor: [
-        'rgba(0, 255, 0, 0.5)',
-      ],
-      borderColor: [
-        'rgb(0, 69, 36)',
-      ],
-      categoryPercentage: 1,
-      borderWidth: 1,
-      barPercentage: 1,
-    }]
-  };
-  var allPerf = {
-                          type: 'bar',
-                          data: data,
-                          options: {
-                              scales: {
-                                    y: {
-                                      beginAtZero: true
-                                    }
-                                  }
-                          }
-                        };
+ let allPerf = typeChartHandler(typeChart, labelsSuccess, 'Success rate', dictSuccess);
+
+
 
   if (perfChartJsCharts["successRateChart"]) perfChartJsCharts["successRateChart"].destroy();
   perfChartJsCharts["successRateChart"] = new Chart(ctx, allPerf);
@@ -144,7 +200,9 @@ dataSuccessRateDict[Date.parse(key)] = parseFloat(obj[key]);
 
 var testCountSelect = document.querySelector("#selectTestCount");
 
-function createTestCountChart(period){
+function createTestCountChart(){
+var period = document.getElementById("selectTestCount").value;
+var typeChart = document.getElementById("selectChartTC").value;
 console.log("period", period)
 var testCount2 = document.querySelector("#testCountData").textContent;
   console.log(testCount2);
@@ -165,22 +223,7 @@ dataTestCountDict[Date.parse(key)] = parseFloat(obj[key]);
   dictTestCount = sortOnKeys(dataTestCountDict, period)[0];
   labelsTestCount = sortOnKeys(dataTestCountDict, period)[1];
 
-const dataTestCount = {
-  labels: labelsTestCount,
-  datasets: [{
-    label: 'Test Count',
-    data: dictTestCount,
-    borderColor: [
-      'rgba(0, 180, 33, 1)',
-    ],
-    tension: 0.1
-
-  }]
-};
-var settingsTestCount = {
-                        type: 'line',
-                        data: dataTestCount,
-                      };
+let settingsTestCount = typeChartHandler(typeChart, labelsTestCount, 'Test Count', dictTestCount);
 
   if (perfChartJsCharts["testCountChart"]) perfChartJsCharts["testCountChart"].destroy();
   perfChartJsCharts["testCountChart"] = new Chart(ctxTestCount, settingsTestCount);
@@ -192,7 +235,9 @@ var settingsTestCount = {
 
 var buildDurationSelect = document.querySelector("#selectBuildDuration");
 
-function createBuildDurationChart(period){
+function createBuildDurationChart(){
+var period = document.getElementById("selectBuildDuration").value;
+var typeChart = document.getElementById("selectChartBD").value;
 console.log("period", period)
 var buildDuration = document.querySelector("#buildDurationData").textContent;
   console.log(buildDuration);
@@ -214,22 +259,8 @@ dataBuildDurationDict[Date.parse(key)] = parseFloat(obj[key]);
   dictBuildDuration = sortOnKeys(dataBuildDurationDict, period)[0];
   labelsBuildDuration = sortOnKeys(dataBuildDurationDict, period)[1];
 
-const dataBuildDuration = {
-  labels: labelsBuildDuration,
-  datasets: [{
-    label: 'Build duration',
-    data: dictBuildDuration,
-    borderColor: [
-      'rgba(0, 180, 33, 1)',
-    ],
-    tension: 0.1
 
-  }]
-};
-var settingsBuildDuration = {
-                        type: 'line',
-                        data: dataBuildDuration,
-                      };
+   let settingsBuildDuration = typeChartHandler(typeChart, labelsBuildDuration, 'Build duration', dictBuildDuration);
   if (perfChartJsCharts["buildDurationChart"]) perfChartJsCharts["buildDurationChart"].destroy();
   perfChartJsCharts["buildDurationChart"] = new Chart(ctxBuild, settingsBuildDuration);
 
@@ -241,7 +272,9 @@ var settingsBuildDuration = {
 
 var artifactSizeSelect = document.querySelector("#selectArtifactSize");
 
-function createArtifactSizeChart(period){
+function createArtifactSizeChart(){
+var period = document.getElementById("selectArtifactsSize").value;
+var typeChart = document.getElementById("selectChartAS").value;
 console.log("period", period)
 var artifactSize = document.querySelector("#artifactSizeData").textContent;
   console.log(artifactSize);
@@ -263,22 +296,7 @@ dataArtifactSizeDict[Date.parse(key)] = parseFloat(obj[key]);
   dictArtifactSize = sortOnKeys(dataArtifactSizeDict, period)[0];
   labelsArtifactSize = sortOnKeys(dataArtifactSizeDict, period)[1];
 
-const dataArtifactSize = {
-  labels: labelsArtifactSize,
-  datasets: [{
-    label: 'Artifact Size',
-    data: dictArtifactSize,
-    borderColor: [
-      'rgba(0, 180, 33, 1)',
-    ],
-    tension: 0.1
-
-  }]
-};
-var settingsArtifactSize = {
-                        type: 'line',
-                        data: dataArtifactSize,
-                      };
+let settingsArtifactSize = typeChartHandler(typeChart, labelsArtifactSize, 'Artifact Size', dictArtifactSize);
   if (perfChartJsCharts["artifactSizeChart"]) perfChartJsCharts["artifactSizeChart"].destroy();
   perfChartJsCharts["artifactSizeChart"] = new Chart(ctxArtifactsSize, settingsArtifactSize);
 
@@ -288,7 +306,9 @@ var settingsArtifactSize = {
 
 var timeQueueSelect = document.querySelector("#selectTimeQueue");
 
-function createTimeQueueChart(period){
+function createTimeQueueChart(){
+var period = document.getElementById("selectTimeQueue").value;
+var typeChart = document.getElementById("selectChartTQ").value;
 console.log("period", period)
 var timeQueue = document.querySelector("#timeQueueData").textContent;
   console.log(timeQueue);
@@ -310,22 +330,7 @@ dataTimeQueueDict[Date.parse(key)] = parseFloat(obj[key]);
   dictTimeQueue = sortOnKeys(dataTimeQueueDict, period)[0];
   labelsTimeQueue = sortOnKeys(dataTimeQueueDict, period)[1];
 
-const dataTimeQueue = {
-  labels: labelsTimeQueue,
-  datasets: [{
-    label: 'Time Spent In Queue',
-    data: dictTimeQueue,
-    borderColor: [
-      'rgba(0, 180, 33, 1)',
-    ],
-    tension: 0.1
-
-  }]
-};
-var settingsTimeQueue = {
-                        type: 'line',
-                        data: dataTimeQueue,
-                      };
+   let settingsTimeQueue = typeChartHandler(typeChart, labelsTimeQueue, 'Time Spent In Queue', dictTimeQueue);
   if (perfChartJsCharts["timeQueueChart"]) perfChartJsCharts["timeQueueChart"].destroy();
   perfChartJsCharts["timeQueueChart"] = new Chart(ctxTimeSpentQueue, settingsTimeQueue);
 
