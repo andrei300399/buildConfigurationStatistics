36c85e85578794c21dd45b35b8667bd28863d27f
Kukhto <Andrey.Kukhto@nexign.com>
2024-02-07 19:30:15 +0300

    add build date xml handler all

--- parsrXmlBuilds.py
+++ parsrXmlBuilds.py
@@ -2,13 +2,18 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 import dateutil.relativedelta
 import re
+from random import randrange
 
 curr_dt = datetime.now()
 
 len_builds = 18
 
+path_to_builds = "C:\\buildConfigurationStatistics\\work\\jobs\\test-Build-frontend-maven-plugin\\builds\\"
+
+name_file_build = "build.xml"
+
 for i in range(0, len_builds):
-    root = ET.parse(f"C:\\buildConfigurationStatistics\\work\\jobs\\test-Build-frontend-maven-plugin\\builds\\{i + 1}\\build.xml")
+    root = ET.parse(f"{path_to_builds}{i + 1}\\{name_file_build}")
 
     print("Current datetime: ", curr_dt)
     timestamp = int(round(curr_dt.timestamp()))*1000
@@ -28,14 +33,20 @@ for i in range(0, len_builds):
     print(timestampText)
     root.find('timestamp').text = str(timestamp_new)
 
-    root.write(f'C:\\buildConfigurationStatistics\\work\\jobs\\test-Build-frontend-maven-plugin\\builds\\{i + 1}\\build_new.xml', encoding = "UTF-8", xml_declaration = True)
+    startTimeText = new_root.find('startTime').text
+    print(startTimeText)
+    rand_milli = randrange(5, 25)
+    root.find('startTime').text = str(timestamp_new + rand_milli)
+    print(rand_milli)
+
+    root.write(f'{path_to_builds}{i + 1}\\{name_file_build}', encoding = "UTF-8", xml_declaration = True)
 
     # rewrire version xml
 
-    with open(f"C:\\buildConfigurationStatistics\\work\\jobs\\test-Build-frontend-maven-plugin\\builds\\{i + 1}\\build_new.xml", "r") as rfile:
+    with open(f"{path_to_builds}{i + 1}\\{name_file_build}", "r") as rfile:
         s = rfile.read()
         rplce = re.sub('version=\'1.0\'', "version=\'1.1\'", s)
-    with open(f"C:\\buildConfigurationStatistics\\work\\jobs\\test-Build-frontend-maven-plugin\\builds\\{i + 1}\\build_new.xml", "w") as wfile:
+    with open(f"{path_to_builds}{i + 1}\\{name_file_build}", "w") as wfile:
         wfile.write(rplce)
 
-### 1706547093301
+### 1707287342000
